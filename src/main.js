93128bf00c2bc88c9d57108b8a1743e9d273b165
13671052669@139.com <NoAccess2Pc>
2019-09-28 17:28:55 +0800

    websocket 心跳检测封装

--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@ import App from './App.vue'
 import Antd from 'ant-design-vue'
 import "ant-design-vue/dist/antd.css"
 import store from '@/store'
-import router from  '@/router'
+import router from '@/router'
+
 Vue.config.productionTip = false
 Vue.use(Antd)
 
+// const socketIO = new VueSocketIO({
+//     debug: false,
+//     connection: 'ws://localhost:9000',
+//     vuex: {
+//         store,
+//         actionPrefix: 'SOCKET_',
+//         mutationPrefix: 'SOCKET_'
+//     },
+//     options: {path: "/api/v1/ws"}
+// })
 
 new Vue({
-  router,
-  store,
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app')
--- src/views/logistic/index.vue
+++ src/views/logistic/index.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="logistic">
         <a-button class="editable-add-btn" @click="add">新增</a-button>
-        <a-button class="editable-add-btn" @click="getData">temp</a-button>
+        <a-button class="editable-add-btn" @click="printInfo">temp</a-button>
+        <a-button class="editable-add-btn" @click="printInfo">print</a-button>
         <a-table
                 :columns="columns"
                 :rowKey="record => record.id"
@@ -16,50 +17,12 @@
 </template>
 
 <script>
+    import wsocket from '@/utils/wstools'
+
     export default {
         name: "index",
-        created () {
-            //页面刚进入时开启长连接
-            this.initWebSocket()
-        },
-        destroyed: function () {
-            //页面销毁时关闭长连接
-            this.websocketclose();
-        },
+
         methods: {
-            initWebSocket () { //初始化weosocket
-
-                const wsuri = process.env.WS_API + "/websocket/threadsocket";//ws地址
-                this.websock = new WebSocket(wsuri);
-                this.websocket.onopen = this.websocketonopen;
-
-                this.websocket.onerror = this.websocketonerror;
-
-                this.websock.onmessage = this.websocketonmessage;
-                this.websock.onclose = this.websocketclose;
-            },
-
-            websocketonopen () {
-                console.log("WebSocket连接成功");
-            },
-            websocketonerror (e) { //错误
-                console.log("WebSocket连接发生错误");
-            },
-            websocketonmessage (e) { //数据接收
-                const redata = JSON.parse(e.data);
-                //注意：长连接我们是后台直接1秒推送一条数据，
-                //但是点击某个列表时，会发送给后台一个标识，后台根据此标识返回相对应的数据，
-                //这个时候数据就只能从一个出口出，所以让后台加了一个键，例如键为1时，是每隔1秒推送的数据，为2时是发送标识后再推送的数据，以作区分
-                console.log(redata.value);
-            },
-
-            websocketsend (agentData) {//数据发送
-                this.websock.send(agentData);
-            },
-
-            websocketclose (e) { //关闭
-                console.log("connection closed (" + e.code + ")");
-            },
             add () {
             },
             update () {
@@ -76,15 +39,27 @@
                     console.log(err)
                 })
             },
+            printInfo () {
+                console.log(this.ws)
+            },
+        }
+        ,
+        beforeDestroy () {
+            this.ws.close();
+            this.ws = Object;
         },
+        computed: {},
+        watch: {}
+        ,
         data () {
             return {
+                ws: wsocket,
                 data: [],
                 columns: [{
                     title: 'No.',
                     width: '1%',
-                    dataIndex: 'no',
-                    key: 'no',
+                    dataIndex: 'id',
+                    key: 'id',
                     slots: {title: 'no'},
                     scopedSlots: {customRender: 'no'},
                 }, {
